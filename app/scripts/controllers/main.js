66ff78aaafbf570034b6a1eab8ebb46321f92954
Oscar Barba <oscarb@mty.ipointsystems.com>
2016-07-27 17:39:51 -0500

    base layout, education card

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,15 @@
  * Controller of the oscarApp
  */
 angular.module('oscarApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($http, $scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $http.get("info.js")
+      .then(function(response) {
+        $scope.info = response.data;
+    });
   });
