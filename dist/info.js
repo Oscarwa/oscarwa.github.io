d76aa660d5c605e8b19899c3e03c29ad81158051
Oscar Barba <oscarb@mty.ipointsystems.com>
2016-08-30 16:48:41 -0500

    Added initial page

--- dist/info.js
+++ dist/info.js
@@ -18,5 +18,19 @@
       "endYear": 2006,
       "description": ""
     }
-  ]
+  ],
+  "personal": {
+
+    "bio": "I have over 8 years of experience developing Enterprise applications. He is an experienced, self-motivated, result-driven, flexible professional guy with a passion for programming who specializes in turning business ideas into software solutions. Strengths: C#, Javascript, jQuery, HTML5.",
+    "note": "I was the kind of guy who got paid on school to do others homework or final projects.",
+    "dob": "1988-10-18",
+    "nationality": "Mexican",
+    "github": {
+      "label": "Oscarwa",
+      "url": "https://github.com/Oscarwa/"
+    }
+    "linkedin": {
+      "label": "Oscar Barba",
+      "url": "https://www.linkedin.com/in/oscar-barba-b3836531"
+    }
 }
